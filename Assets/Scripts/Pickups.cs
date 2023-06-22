e127b9a000d3330331706f06c2ef1f61e85cbab1
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-06-22 18:38:52 -0400

    NINJA RUN PY
    NOT DONE

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public ParticleSystem Pickup;
+    void Start()
+    {
+        Pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+            Pickup.Play();
+        }
+    }
+}
